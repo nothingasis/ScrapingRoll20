1f17096137174018148d7081e85818e396958420
Nurthin Aziz <nurthinaziz@gmail.com>
2020-11-24 22:50:21 -0800

    needs cleaning

--- D20Soup.py
+++ D20Soup.py
@@ -3,16 +3,29 @@ try:
 except ImportError:
     from bs4 import BeautifulSoup
 
-
-def match_class(target):
-    def do_match(tag):
-        classes = tag.get('class', [])
-        return all(c in classes for c in target)
-    return do_match
+import re
 
 with open("assetLibrary.html") as fp:
     soup = BeautifulSoup(fp, 'html.parser')
     # print soup.find_all(match_class(["feeditemcontent", "cxfeeditemcontent"]))
-    for tag in soup.find_all("li", {"class": "library-item"}):
-        name = tag.find("div", {"class": "namecontainer"})        
-        print("Name: ", name)
+    for folder in soup.find_all("li", {"class": "dd-folder"}):
+        # Get the parent folder.
+        folder_name = folder.find("div", {"class": "folder-title"})
+        print("Folder:", folder_name.text)
+
+        # Get all the children
+        listItems = folder.find_all("li", {"class": "library-item"})
+        for item in listItems:
+            name = item.find("div", {"class": "namecontainer"})
+            print ("{0}".format(name.text))
+            regex = r"data-fullsizeurl=\"(https:\/\/s3\.amazonaws\.com\/files\.d20\.io\/marketplace\/\d+\/[\d\w-]+\/max.png\?\d+)\""
+
+            test_str = "data-fullsizeurl=\"https://s3.amazonaws.com/files.d20.io/marketplace/14665/l6B57dHOGekSHDGEnE4UGg/max.png?1366969146\""
+
+            matches = re.finditer(regex, test_str)
+
+            for matchNum, match in enumerate(matches, start=1):
+                for groupNum in range(0, len(match.groups())):
+                    groupNum = groupNum + 1
+                    url= ("{group}".format(groupNum = groupNum, start = match.start(groupNum), end = match.end(groupNum), group = match.group(groupNum)))
+                    print(url)
